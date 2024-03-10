ac292e6540707c68042fcf303414f64b4d514e08
kubo990 <jakub.vojtasak@gmail.com>
2024-03-10 20:58:27 +0100

    init commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,104 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import React from 'react';
+import { Controller, useForm } from "react-hook-form"
+import axios from 'axios';
+import { Button, Grid, InputLabel, TextField } from '@mui/material';
+import './page.css';
+
+type LoginValues = {
+  email: string;
+  password: string;
+};
+
+const LoginForm: React.FC = () => {
+  const { control, register, handleSubmit, formState: { errors } } = useForm<LoginValues>();
+
+  const onSubmit = async (data: LoginValues) => {
+    try {
+      //const response = await axios.post('user/login', data);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className='tb-login-content'>
+      <div className='tb-login-header'>
+        <h1>Prihlásenie</h1>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)} className='tb-form'>
+        <Grid container spacing={2} direction="column">
+          <Grid item>
+            <InputLabel className='tb-form-label'>Email</InputLabel>
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              rules={{ 
+                required: 'E-mail je povinný.',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'E-mailová adresa nie je validná.'
+                }
+              }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  error={!!errors.email}
+                  helperText={errors.email ? errors.email.message : ''}
+                  InputProps={{
+                    className:"tb-text-field__input"
+                  }}
+                />
+              )}
             />
-          </a>
+          </Grid>
+          <Grid item>
+            <InputLabel className='tb-form-label'>Heslo</InputLabel>
+
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              rules={{ 
+                required: 'Heslo je povinné.',
+                minLength: {
+                  value: 8,
+                  message: 'Heslo musí mať aspoň 8 znakov.'
+                },
+                validate: {
+                  containsUppercase: value =>
+                    /[A-Z]/.test(value) || 'Heslo musí obsahovať aspoň jedno veľké písmeno.',
+                  containsNumber: value =>
+                    /\d/.test(value) || 'Heslo musí obsahovať aspoň jedno číslo.',
+                  containsSpecialCharacter: value =>
+                    /[^a-zA-Z\d]/.test(value) || 'Heslo musí obsahovať aspoň jeden špeciálny znak.'
+                }
+              }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  type="password"
+                  error={!!errors.password}
+                  helperText={errors.password ? errors.password.message : ''}
+                  InputProps={{
+                    className:"tb-text-field__input"
+                  }}
+                />
+              )}
+            />
+          </Grid>
+        </Grid>
+        <div className='mg-top-60'>
+          <Button type="submit" variant="contained" color="primary" className="tb-button">
+            Prihlásiť
+          </Button>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </form>
+    </div>
   );
-}
+};
+
+export default LoginForm;
